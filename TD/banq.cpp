53d16e1aad962ed7287862bc92885c3c137693e3
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2021-10-25 21:36:00 +0100

    updates TD files

--- TD/banq.cpp
+++ TD/banq.cpp
@@ -1,1 +1,43 @@
 #include"banq.hpp"
+void Compte::saisie()
+{
+    cout<<"donner le num de compte : ";
+    cin>>num_C;
+    cout<<"donner le nom du proprietere : ";
+    cin>>nom_C;
+    cout<<"donner le solde : ";
+    cin>>Solde;
+}
+ bool Compte::retirerArgent(float montant_R)
+ {
+     if(Solde>=montant_R)
+     {
+         Solde-=montant_R;
+         cout<<"operation effectue avec succee !"<<endl;
+         return true;
+     }
+     else
+     {
+         cout<<"solde insufisant !"<<endl;
+        return false;
+     }
+ }
+ void Compte::deposerArgent(float sA)
+ {
+     Solde+=sA;
+     cout<<"operation effectue avec succee !"<<endl;
+ }
+ void Compte::consulterSolde() const
+ {
+     cout<<"vous avez comme solde : "<<Solde<<"Dt"<<endl;
+ }
+ void Compte::transfererArgent(float ST, Compte& cc)
+ {
+     if(retirerArgent(ST))
+     {
+         cc.deposerArgent(ST);
+         cout<<"operation effectue avec succee !"<<endl;
+     }
+     else
+        cout<<"solde insufisant !"<<endl;
+ }
--- TD/banq.hpp
+++ TD/banq.hpp
@@ -15,21 +15,21 @@ class Compte
  public :
  //getters
     int getNumCompte()const
-    {return numCompte;}
+    {return num_C;}
     string getNomProp()const
-    {return nomProp;}
+    {return nom_C;}
     float getSolde()const
-    {return solde;}
+    {return Solde;}
  //setters
-    void setNumCompte(int val){numCompte=val;}
-    void setNomProp(string val){nomProp=val;}
-    void setSolde(float val){solde=val;}
+    void setNumCompte(int val){num_C=val;}
+    void setNomProp(string val){nom_C=val;}
+    void setSolde(float val){Solde=val;}
  //Méthode
     void saisie();
     bool retirerArgent(float montant_R);
     void deposerArgent(float sA);
     void consulterSolde() const;
-    void transfererArgent(float ST, int& );
+    void transfererArgent(float ST, Compte& cc );
 };
 
 #endif
